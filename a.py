84df7075aa2118944765df66a5d6fd53930f827f
Thassilo-droid <Thassilo.horn@gmx.de>
2021-11-04 17:29:46 +0100

    ascii

---
+++ a.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+def asciimation():
+    while 1:
+        os.system('cls')
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+
+"""@%#*+=-:. """
+
+
+        time.sleep(0.1)
+
+
+        os.system('cls')
+        print("")
+        print("")
+        print("")
+        time.sleep(0.1)
+
+asciimation()
